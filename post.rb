f30b4ec940b5195cd54e950479dfc469b860dcfe
Giorgi <51827914+GiorgiMazm@users.noreply.github.com>
2021-08-06 10:20:53 +0200

    Add files via upload

---
+++ post.rb
@@ -0,0 +1,34 @@
+class Post 
+    attr_accessor :title, :sub_title, :author, :text, :categorie, creation_date
+
+    def initialize(title, text)
+        @title = title
+        @text = text
+        @categorie = "News"
+      end
+
+    def add_title(title) 
+        @title = title
+    end
+
+    def add_sub_title(sub_title) 
+        @sub_title = sub_title
+    end
+
+    def add_author(author)
+        @author = author
+    end
+
+    def add_text(text) 
+        @text = text
+    end
+
+    def add_categorie(categorie) 
+        @categorie = categorie
+    end
+
+    def add_creation_date(creation_date) 
+        @creation_date = creation_date
+    end
+
+end
